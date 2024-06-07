c7eb36e0a2563e6044ac3678ad182f7c2e72df82
Humano3 <155685318+Humano3@users.noreply.github.com>
2024-06-07 02:35:17 +0000

    Añadi el codigo de la letra I de solid

--- solid.php
+++ solid.php
@@ -43,3 +43,51 @@ $userWithEmailNotification->performAction();
 $smsNotification = new SMSNotification();
 $userWithSMSNotification = new User($smsNotification);
 $userWithSMSNotification->performAction();
+
+
+///Letra I
+<?php
+
+// Interfaces específicas siguiendo el principio de segregación de interfaces
+interface Workable {
+    public function work();
+}
+
+interface Eatable {
+    public function eat();
+}
+
+interface Sleepable {
+    public function sleep();
+}
+
+// Implementación de un trabajador humano que necesita todas las interfaces
+class HumanWorker implements Workable, Eatable, Sleepable {
+    public function work() {
+        echo "Human working...\n";
+    }
+
+    public function eat() {
+        echo "Human eating...\n";
+    }
+
+    public function sleep() {
+        echo "Human sleeping...\n";
+    }
+}
+
+// Implementación de un trabajador robot que solo necesita la interfaz Workable
+class RobotWorker implements Workable {
+    public function work() {
+        echo "Robot working...\n";
+    }
+}
+
+// Ejecución de ejemplos
+$human = new HumanWorker();
+$human->work();
+$human->eat();
+$human->sleep();
+
+$robot = new RobotWorker();
+$robot->work();
